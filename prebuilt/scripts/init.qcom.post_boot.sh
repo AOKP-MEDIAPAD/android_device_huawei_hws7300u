97361b4fa7473c62735202e271e9e056b717fa65
user <user@test.(none)>
2012-10-11 10:56:24 +0400

    build 6.10

--- prebuilt/scripts/init.qcom.post_boot.sh
+++ prebuilt/scripts/init.qcom.post_boot.sh
@@ -89,8 +89,10 @@ case "$target" in
 	 echo 0 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
 	 echo 5 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential 
-	 echo 192000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	 echo 192000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+	 echo 1188000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+	 echo 1188000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 	 chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
