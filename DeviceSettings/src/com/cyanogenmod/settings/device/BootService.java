ca67fc0ad71e0d80ad96b32b387b7057b40691d1
Oleg Semenov <izzzum@gmail.com>
2013-02-03 15:26:44 +0300

    Mb sdcard ext storage fix ( no test )

--- DeviceSettings/src/com/cyanogenmod/settings/device/BootService.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/BootService.java
@@ -51,7 +51,7 @@ public class BootService extends Service  {
                 if( SystemProperties.get(DeviceSettings.PROP_EXT_INTERNAL,"0").equals("1"))
                 {
                     CMDProcessor cmd = new CMDProcessor();
-                    cmd.rootCommand("mount -t vfat -o umask=0000 /dev/block/vold/179:97 /storage/sdcard0");
+                    cmd.rootCommand("mount -t vfat -o umask=0000 /dev/block/vold/179:97 /storage/emulated/legacy");
                     cmd.rootCommand("mount -o bind /data/media /storage/sdcard1");
                     cmd.rootCommand("chmod 755 /storage/sdcard1");
                 }
