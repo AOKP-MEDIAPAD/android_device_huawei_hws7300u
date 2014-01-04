1039eb3502ddcaa2c63838e3f371779168300c01
zyr3x <izzzum@gmail.com>
2014-01-04 15:22:16 +0600

    Update bluedroid

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,5 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Huawei MediaPad"
-#define BTA_DISABLE_DELAY 5000 /* in milliseconds */
-#define BT_TRACE_VERBOSE  TRUE
-#define RPC_TRACE_ONLY  TRUE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #endif
