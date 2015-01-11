afc556b82e0ce3b44f3d2c96fb468ac2a3fd7a36
zyr3x <izzzum@gmail.com>
2015-01-11 09:38:41 +0300

    Update RIL - Fixed compile build - Fixed sim detect - Fixed gsm call

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,6 +1,7 @@
 # Copyright 2006 The Android Open Source Project
 
 ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -17,34 +18,25 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
-
-ifeq ($(SIM_COUNT), 2)
-    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
+LOCAL_CFLAGS :=
+ifdef BOARD_USE_NEW_LIBRIL_HTC
+    LOCAL_CFLAGS += -DNEW_LIBRIL_HTC
 endif
+
+LOCAL_CFLAGS += -DRIL_NO_CELL_INFO_LIST
 
 LOCAL_MODULE:= libril
 
+#USE HCRADIO
+ifeq ($(BOARD_USES_HC_RADIO),true)
+LOCAL_CFLAGS += -DHCRADIO
+endif
+
+ifeq ($(BOARD_USES_LEGACY_RIL),true)
+LOCAL_CFLAGS += -DLEGACY_RIL
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils \
-    librilutils_static
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
+endif # TARGET_BOARD_PLATFORM
 endif # BOARD_PROVIDES_LIBRIL
