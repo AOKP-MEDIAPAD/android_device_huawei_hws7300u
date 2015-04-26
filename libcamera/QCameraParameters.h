2718516e207d79cb86ba6fe6abaf33a8fd13833b
zyr3x <izzzum@gmail.com>
2015-04-26 10:41:23 +0300

    Revert camera api

--- libcamera/QCameraParameters.h
+++ libcamera/QCameraParameters.h
@@ -20,7 +20,7 @@
 #ifndef __QUALCOMM_CAMERA_PARAMETERS__
 #define __QUALCOMM_CAMERA_PARAMETERS__
 
-#include <camera/CameraParameters2.h>
+#include <camera/CameraParameters.h>
 
 using namespace android;
 
@@ -39,7 +39,7 @@ struct FPSRange{
 };
 
 
-class QCameraParameters: public CameraParameters2
+class QCameraParameters: public CameraParameters
 {
 public:
     void setPreviewFrameRateMode(const char *mode);
