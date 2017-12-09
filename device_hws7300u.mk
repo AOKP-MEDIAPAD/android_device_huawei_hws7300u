4f1a3ee9376ffa06854d2e5c194382dfd52033ad
zyr3x <izzzum@gmail.com>
2017-12-09 21:05:40 +0300

    Added cm-12.1-caf

--- device_hws7300u.mk
+++ device_hws7300u.mk
@@ -243,7 +243,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     device/huawei/hws7300u/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 # TWRP
